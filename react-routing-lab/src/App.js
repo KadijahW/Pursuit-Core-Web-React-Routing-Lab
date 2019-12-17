e381f924cbf4156a235de0c3d0e5999ec2aee99e
kadjiah <kadijahwilson@pursuit.org>
2019-12-16 20:57:06 -0500

    set up nav bar and routes

--- react-routing-lab/src/App.js
+++ react-routing-lab/src/App.js
@@ -1,11 +1,25 @@
 import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
+import Dog from './Components/Dog'
+import Cat from './Components/Cat'
+
 
 function App() {
   return (
     <div className="App">
-    
+    <nav>
+      <Link to ="/dog">Dog</Link>
+      <Link to="/cat">Cat</Link>
+    </nav>
+
+    <Switch>
+     <Route path="/dog/random" component={Dog}/>
+     <Route path="dog/random/:num" component={Dog}/>
+     <Route path="/dog/:breed" component={Dog}/>
+     <Route path="/cat/random" component={Cat}/>
+     <Route path="/cat/random/:num" component={Cat}/>
+    </Switch>
     </div>
   );
 }
